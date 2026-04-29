765ab1c48cc52635ef8cb225764db65cca8986ae
Username11-stack <anchitpathak2000@gmail.com>
2026-04-29 20:09:18 +0530

    Added new file model_test on suggestion of copilot to fix the concurrency error which happens in model.py file

---
+++ Config.py
@@ -0,0 +1,274 @@
+import random
+from zipfile import Path
+from typing import List, Dict
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+
+# Set seed
+def view_image(image_path: Path):
+    random.seed(42) # <- try changing this and see what happens
+
+    # 1. Get all image paths (* means "any combination")
+    image_path_list = list(image_path.glob("*/*/*.jpg"))
+
+    # 2. Get random image path
+    random_image_path = random.choice(image_path_list)
+
+    # 3. Get image class from path name (the image class is the name of the directory where the image is stored)
+    image_class = random_image_path.parent.stem
+
+    # 4. Open image
+    img = Image.open(random_image_path)
+
+    # 5. Print metadata
+    print(f"Random image path: {random_image_path}")
+    print(f"Image class: {image_class}")
+    print(f"Image height: {img.height}") 
+    print(f"Image width: {img.width}")
+    img.show()
+
+#--------------------------------------------------------------------------------------------
+
+
+def plot_transformed_images(image_paths, transform, n=3, seed=42):
+    """Plots a series of random images from image_paths.
+
+    Will open n image paths from image_paths, transform them
+    with transform and plot them side by side.
+
+    Args:
+        image_paths (list): List of target image paths. 
+        transform (PyTorch Transforms): Transforms to apply to images.
+        n (int, optional): Number of images to plot. Defaults to 3.
+        seed (int, optional): Random seed for the random generator. Defaults to 42.
+    """
+    random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(f) 
+            ax[0].set_title(f"Original \nSize: {f.size}")
+            ax[0].axis("off")
+
+            # Transform and plot image
+            # Note: permute() will change shape of image to suit matplotlib 
+            # (PyTorch default is [C, H, W] but Matplotlib is [H, W, C])
+            transformed_image = transform(f).permute(1, 2, 0) 
+            ax[1].imshow(transformed_image) 
+            ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
+            ax[1].axis("off")
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+            plt.show()
+
+#--------------------------------------------------------------------------------------------
+
+def display_random_images(dataset: torch.utils.data.dataset.Dataset,
+                          classes: List[str] = None,
+                          n: int = 10,
+                          display_shape: bool = True,
+                          seed: int = None):
+    
+    # 2. Adjust display if n too high
+    if n > 10:
+        n = 10
+        display_shape = False
+        print(f"For display purposes, n shouldn't be larger than 10, setting to 10 and removing shape display.")
+    
+    # 3. Set random seed
+    if seed:
+        random.seed(seed)
+
+    # 4. Get random sample indexes
+    random_samples_idx = random.sample(range(len(dataset)), k=n)
+
+    # 5. Setup plot
+    plt.figure(figsize=(16, 8))
+
+    # 6. Loop through samples and display random samples 
+    for i, targ_sample in enumerate(random_samples_idx):
+        targ_image, targ_label = dataset[targ_sample][0], dataset[targ_sample][1]
+
+        # 7. Adjust image tensor shape for plotting: [color_channels, height, width] -> [color_channels, height, width]
+        targ_image_adjust = targ_image.permute(1, 2, 0)
+
+        # Plot adjusted samples
+        plt.subplot(1, n, i+1)
+        plt.imshow(targ_image_adjust)
+        plt.axis("off")
+        if classes:
+            title = f"class: {classes[targ_label]}"
+            if display_shape:
+                title = title + f"\nshape: {targ_image_adjust.shape}"
+        plt.title(title)
+    plt.show()
+
+#--------------------------------------------------------------------------------------------
+
+def train_step(model: torch.nn.Module, 
+               dataloader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               optimizer: torch.optim.Optimizer):
+    # Put model in train mode
+    model.train()
+    
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+    
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate  and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item() 
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Calculate and accumulate accuracy metrics across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+    # Adjust metrics to get average loss and accuracy per batch 
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+#--------------------------------------------------------------------------------------------
+
+def test_step(model: torch.nn.Module, 
+              dataloader: torch.utils.data.DataLoader, 
+              loss_fn: torch.nn.Module):
+    # Put model in eval mode
+    model.eval() 
+    
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+    
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+    
+            # 1. Forward pass
+            test_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+            
+            # Calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+            
+    # Adjust metrics to get average loss and accuracy per batch 
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+#--------------------------------------------------------------------------------------------
+
+# 1. Take in various parameters required for training and test steps
+def train(model: torch.nn.Module, 
+          train_dataloader: torch.utils.data.DataLoader, 
+          test_dataloader: torch.utils.data.DataLoader, 
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5):
+    
+    # 2. Create empty results dictionary
+    results = {"train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": []
+    }
+    
+    # 3. Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer)
+        test_loss, test_acc = test_step(model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn)
+        
+        # 4. Print out what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # 5. Update results dictionary
+        # Ensure all data is moved to CPU and converted to float for storage
+        results["train_loss"].append(train_loss.item() if isinstance(train_loss, torch.Tensor) else train_loss)
+        results["train_acc"].append(train_acc.item() if isinstance(train_acc, torch.Tensor) else train_acc)
+        results["test_loss"].append(test_loss.item() if isinstance(test_loss, torch.Tensor) else test_loss)
+        results["test_acc"].append(test_acc.item() if isinstance(test_acc, torch.Tensor) else test_acc)
+
+    # 6. Return the filled results at the end of the epochs
+    return results
+
+#--------------------------------------------------------------------------------------------
+
+def plot_loss_curves(results: Dict[str, List[float]]):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+    """
+    
+    # Get the loss values of the results dictionary (training and test)
+    loss = results['train_loss']
+    test_loss = results['test_loss']
+
+    # Get the accuracy values of the results dictionary (training and test)
+    accuracy = results['train_acc']
+    test_accuracy = results['test_acc']
+
+    # Figure out how many epochs there were
+    epochs = range(len(results['train_loss']))
+
+    # Setup a plot 
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, test_loss, label='test_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, test_accuracy, label='test_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.show()
--- Model.py
+++ Model.py
@@ -6,21 +6,25 @@ import os
 import pandas as pd
 import tabulate as tb
 import sklearn
+from pathlib import Path
+import Config
+import torchinfo
+from torchinfo import summary
 
-    
+
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-data_path = "C:/Users/anchi/GitHub/Repos/Image_Classification/Data/Training_set.csv"
-#image = data_path / "images"
+data_path = Path("C:/Users/anchi/GitHub/Repos/Image_Classification/Data_Refined")
+image_path_list = list(data_path.glob("*/*/*.jpg"))
 
-df = pd.read_csv(data_path)
-print(tb.tabulate(df.head(100), headers='keys', tablefmt='psql'))
-print(df["label"].nunique())
+train_dir = os.path.join(data_path, "Train")
+test_dir = os.path.join(data_path, "Test")
 
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
 
-# Write transform for image
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
 data_transform = transforms.Compose([
     # Resize the images to 64x64
     transforms.Resize(size=(64, 64)),
@@ -29,3 +33,164 @@ data_transform = transforms.Compose([
     # Turn the image into a torch.Tensor
     transforms.ToTensor() # this also converts all pixel values from 0 to 255 to be between 0.0 and 1.0 
 ])
+
+# Config.plot_transformed_images(image_paths=list(data_path.glob("*/*/*.jpg")),transform=data_transform)
+
+
+# Use ImageFolder to create dataset(s)
+from torchvision import datasets
+
+train_data = datasets.ImageFolder(root=train_dir, # target folder of images
+                                  transform=data_transform, # transforms to perform on data (images)
+                                  target_transform=None) # transforms to perform on labels (if necessary)
+
+test_data = datasets.ImageFolder(root=test_dir, 
+                                 transform=data_transform)
+
+class_names = train_data.classes
+class_dict = train_data.class_to_idx
+print(f"Class names: {class_names}")
+print(f"Class to index mapping: {class_dict}")
+
+img, label = train_data[0][0], train_data[0][1]
+print(f"Image tensor:\n{img}")
+print(f"Image shape: {img.shape}")
+print(f"Image datatype: {img.dtype}")
+print(f"Image label: {label}")
+print(f"Label datatype: {type(label)}")
+
+
+from torch.utils.data import DataLoader
+
+BATCH_SIZE = 32
+NUM_WORKERS = os.cpu_count()
+
+train_dataloader = DataLoader(dataset=train_data, 
+                              batch_size=BATCH_SIZE, # how many samples per batch?
+                              num_workers=NUM_WORKERS, # how many subprocesses to use for data loading? (higher = more)
+                              shuffle=True) # shuffle the data?
+
+test_dataloader = DataLoader(dataset=test_data, 
+                             batch_size=BATCH_SIZE, 
+                             num_workers=NUM_WORKERS, 
+                             shuffle=False) # don't usually need to shuffle testing data
+
+# Config.display_random_images(dataset=train_data, classes=class_names, n=10, seed=42)
+
+'''img, label = next(iter(train_dataloader))
+
+# Batch size will now be 1, try changing the batch_size parameter above and see what happens
+print(f"Image shape: {img.shape} -> [batch_size, color_channels, height, width]")
+print(f"Label shape: {label.shape}")'''
+
+
+class TinyVGG(nn.Module):
+
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1), # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from? 
+            # It's because each layer of our network compresses and changes the shape of our input data.
+            nn.Linear(in_features=hidden_units*16*16,
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        # print(x.shape)
+        x = self.conv_block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
+        # return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
+
+torch.manual_seed(42)
+model_0 = TinyVGG(input_shape=3, # number of color channels (3 for RGB) 
+                  hidden_units=10, 
+                  output_shape=len(train_data.classes)).to(device)
+print(model_0)
+
+'''
+# Try a forward pass on a single image (to test the model)
+
+# 1. Get a batch of images and labels from the DataLoader
+img_batch, label_batch = next(iter(train_dataloader))
+
+# 2. Get a single image from the batch and unsqueeze the image so its shape fits the model
+img_single, label_single = img_batch[0].unsqueeze(dim=0), label_batch[0]
+print(f"Single image shape: {img_single.shape}\n")
+
+# 3. Perform a forward pass on a single image
+model_0.eval()
+with torch.inference_mode():
+    pred = model_0(img_single.to(device))
+    
+# 4. Print out what's happening and convert model logits -> pred probs -> pred label
+print(f"Output logits:\n{pred}\n")
+print(f"Output prediction probabilities:\n{torch.softmax(pred, dim=1)}\n")
+print(f"Output prediction label:\n{torch.argmax(torch.softmax(pred, dim=1), dim=1)}\n")
+print(f"Actual label:\n{label_single}")
+'''
+
+summary(model_0, input_size=[1, 3, 64, 64]) # do a test pass through of an example input size
+
+#----------------------------------------------------------------------------------------
+#--------------------------MODEL TRAINING AND TESTING FUNCTIONS----------------------------------
+#----------------------------------------------------------------------------------------
+
+# Set random seeds
+torch.manual_seed(42) 
+torch.cuda.manual_seed(42)
+
+# Set number of epochs
+NUM_EPOCHS = 5
+
+# Recreate an instance of TinyVGG
+model_0 = TinyVGG(input_shape=3, # number of color channels (3 for RGB) 
+                  hidden_units=10, 
+                  output_shape=len(train_data.classes)).to(device)
+
+# Setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+
+# Start the timer
+from timeit import default_timer as timer 
+start_time = timer()
+
+# Train model_0 
+model_0_results = Config.train(model=model_0, 
+                        train_dataloader=train_dataloader,
+                        test_dataloader=test_dataloader,
+                        optimizer=optimizer,
+                        loss_fn=loss_fn, 
+                        epochs=NUM_EPOCHS)
+
+# End the timer and print out how long it took
+end_time = timer()
+print(f"Total training time: {end_time-start_time:.3f} seconds")
